c25a36ebd13742093f216573260b3d8add8d18b1
kalinplus <13658226+kalinplus@user.noreply.gitee.com>
2025-05-29 18:00:16 +0800

    chore: type check

--- src/components/Conversation.vue
+++ src/components/Conversation.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+// @ts-nocheck
 import { ref, onMounted, onUnmounted, nextTick, watch } from "vue";
 import { useI18n } from "vue-i18n";
 import { useRouter } from "vue-router";
